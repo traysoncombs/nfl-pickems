177042944930b43b9e769ce5c46a8f1cda52d82d
trayson <trayson>
2021-10-06 15:02:39 +0000

    ahhhhhh

--- modules/classes/Leaderboard.php
+++ modules/classes/Leaderboard.php
@@ -55,7 +55,7 @@ class Leaderboard {
     foreach ($this->usernames as $user){
       $tmp_usernames[$user] = $this->count_wins($user);
     }
-    var_dump($tmp_usernames);
+    var_dump(string($tmp_usernames));
     $this->usernames = array_keys(arsort($tmp_usernames));
   }
 
