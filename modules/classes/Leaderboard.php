8d4e29f35bdcbe5953c268b0915fa57ec4c1e29b
trayson <trayson.combs@gmail.com>
2021-11-16 00:30:31 -0600

    Fixed leaderboard

--- modules/classes/Leaderboard.php
+++ modules/classes/Leaderboard.php
@@ -69,8 +69,10 @@ class Leaderboard {
   public function get_weeks(){
     $page = $this->page;
     $start_index = $page == 1 ? 0 : (((count($this->weeks) + $page) * 2) % (count($this->weeks) + 1)); // May or may not work, ig we will see.
-    $end_index = ($start_index + 1) >= count($this->weeks) - 1 ? ($start_index) : ($start_index + 1);
-    return array_slice($this->weeks, $start_index, $start_index + 1);
+    $end_index = ($start_index + 2) > count($this->weeks) ? 1 : 2;
+    var_dump($start_index);
+    var_dump(count($this->weeks));
+    return array_slice($this->weeks, $start_index, $end_index);
   }
 
   public function get_score($username, $week) {
